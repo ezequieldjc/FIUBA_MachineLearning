5581a04fa4bfadcdcda4ae583fdf951e7f3fa185
ezequieldjc <ezequiel.djc@gmail.com>
2021-11-24 19:48:02 -0300

    add test_pandas

---
+++ test_pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+df = pd.read_excel('/Users/ezequieldjemdjemian/Documents/github/FIUBA_MachineLearning/Módulo 00 - Material inicial/clase1.xlsx')
+
+#metodos de dataframe
+    #head()
+    #info()
+    #shape
+    #describe()
+    #tail()
+    #df["nombreColumna"]
+    #set(df["nombreColumna"])
+    
